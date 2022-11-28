71ddca89ad860bd2f3c356290662e2eaf7f1fee8
Vishnu <codemaker2015@gmail.com>
2022-11-28 16:03:14 +0530

    initial commit

---
+++ src/components/utils/_var.js
@@ -0,0 +1,13 @@
+const fonts = {
+  $titleFont: 'Ubuntu, sans-serif',
+  $mainFont: 'Raleway, sans-serif',
+  $latoFont: 'Lato, sans-serif'
+}
+
+const colors = {
+  $colorBg: '#333',
+  $colorCardBg: '#302f34',
+  $colorGold: '#f3f3f3'
+}
+
+export { fonts, colors }
